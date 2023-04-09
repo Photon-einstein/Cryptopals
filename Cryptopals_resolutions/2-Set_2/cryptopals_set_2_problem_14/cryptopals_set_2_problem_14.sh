d0ee77488d2227615a20fbab388e072fe4ebaed6
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2023-04-09 15:04:23 +0100

    Delete Cryptopals_resolutions/2-Set_2/cryptopals_set_2_problem_14 directory

--- Cryptopals_resolutions/2-Set_2/cryptopals_set_2_problem_14/cryptopals_set_2_problem_14.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm ./build/*
-g++ -c ./src/Function.cpp -o ./build/Function.o
-g++ -c ./src/RandomPrefixWorker.cpp -o ./build/RandomPrefixWorker.o
-g++ -Wall -std=c++17 ./src/cryptopals_set_2_problem_14.cpp  ./build/Function.o ./build/RandomPrefixWorker.o -o ./build/cryptopals_set_2_problem_14.o -lcrypto
-./build/cryptopals_set_2_problem_14.o
