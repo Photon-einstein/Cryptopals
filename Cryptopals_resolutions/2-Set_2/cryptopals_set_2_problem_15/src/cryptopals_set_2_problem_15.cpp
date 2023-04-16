5cfb8408793005103c9c78e51c0dbc5ee7debf92
Tiago Sousa <31144077+Photon-einstein@users.noreply.github.com>
2023-04-16 03:53:52 +0100

    Add files via upload

---
+++ Cryptopals_resolutions/2-Set_2/cryptopals_set_2_problem_15/src/cryptopals_set_2_problem_15.cpp
@@ -0,0 +1,53 @@
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <string>
+#include <math.h>
+#include <ctype.h>
+#include <assert.h>
+#include <vector>
+#include <iostream>
+#include <cstddef>
+#include <unordered_map>
+#include <bits/stdc++.h>
+#include <cctype>
+#include <fstream>
+#include <random>
+#include <map>
+#include <algorithm> // for copy() and assign()
+#include <iterator> // for back_inserter
+#include <memory>
+
+#include "./../include/Function.h"
+#include "./../include/Pad.h"
+#include "./../include/PadPKCS_7.h"
+#include "./../include/Test.h"
+
+int main (void) {
+  clock_t start, end;
+  double time;
+  start = clock();
+  /* work to verify */
+  std::shared_ptr<Pad> p = std::make_shared<PadPKCS_7>(blockSize);
+  Test t = Test(p);
+  std::vector<std::string> v;
+  int i;
+  v.push_back("ICE ICE BABY\x04\x04\x04\x04");
+  v.push_back("ICE ICE BABY\x05\x05\x05\x05");
+  v.push_back("ICE ICE BABY\x01\x02\x03\x04");
+  for (i = 0; i < (int)v.size(); ++i) {
+    t.addTest(v[i]);
+  }
+  t.runTests();
+  /* end of the work */
+  end = clock();
+  time = (double)(end - start) / CLOCKS_PER_SEC;
+  printf("\nProgram took %f s.", time);
+  printf("\n");
+  return 0;
+}
+/******************************************************************************/
