d7102318d50a63a7bbda8994e271abca97f14515
Tiago Sousa <t.l.sousa@gmail.com>
2025-05-06 01:33:31 +0200

    Update clang format script

--- Cryptopals_resolutions/4-Set_4/cryptopals_set_4_problem_31/codeFormat.sh
+++ Cryptopals_resolutions/4-Set_4/cryptopals_set_4_problem_31/codeFormat.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # include files
-clang-format -i ./include/HMAC_SHA1.hpp ./include/HMAC.hpp ./include/MessageExtractionFacility.hpp ./include/MessageFormat.hpp ./include/Server.hpp ./include/SHA.hpp ./include/SHA1.hpp
+clang-format -i ./include/Attacker.hpp ./include/HMAC_SHA1.hpp ./include/HMAC.hpp ./include/MessageExtractionFacility.hpp ./include/MessageFormat.hpp ./include/Server.hpp ./include/SHA.hpp ./include/SHA1.hpp
 # src files
-clang-format -i ./src/HMAC_SHA1.cpp ./src/HMAC.cpp ./src/MessageExtractionFacility.cpp ./src/Server.cpp ./src/SHA.cpp ./src/SHA1.cpp ./src/run_attacker.cpp ./src/run_server.cpp
+clang-format -i ./src/Attacker.cpp ./src/HMAC_SHA1.cpp ./src/HMAC.cpp ./src/MessageExtractionFacility.cpp ./src/SHA.cpp ./src/SHA1.cpp ./src/Server.cpp ./src/run_attacker.cpp ./src/run_server.cpp
 # test files
 clang-format -i ./tests/test_hmac_sha1.cpp ./tests/test_server.cpp ./tests/test_sha1.cpp
--- Cryptopals_resolutions/4-Set_4/cryptopals_set_4_problem_31/include/Attacker.hpp
+++ Cryptopals_resolutions/4-Set_4/cryptopals_set_4_problem_31/include/Attacker.hpp
@@ -8,21 +8,18 @@ public:
   ~Attacker();
 
   /**
-   * @brief This method will try to break HMAC SHA1 signature for any given 
+   * @brief This method will try to break HMAC SHA1 signature for any given
    * file.
-   * 
-   * This method will try to break HMAC SHA1 signature for any given 
+   *
+   * This method will try to break HMAC SHA1 signature for any given
    * file, the signature will be validated on the server side.
    */
   void breakHmacSHA1();
-
-
 
 private:
   bool _debugFlag;
   const int _portServerProduction{18080};
   const int _portServerTest{18081};
-
 };
 
 #endif // ATTACKER_HPP
