01ebc937ed97c70fe1ac8a6c71d9f4e50a4fe7c8
Tiago Sousa <t.l.sousa@gmail.com>
2025-06-19 10:17:26 +0200

    Add skelethon problem 34 of set 5

---
+++ Cryptopals_resolutions/5-Set_5/cryptopals_set_5_problem_34/include/EncryptionUtility.hpp
@@ -0,0 +1,77 @@
+#ifndef ENCRYPTION_UTILITY_HPP
+#define ENCRYPTION_UTILITY_HPP
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <memory>
+#include <openssl/bn.h>
+#include <openssl/err.h>
+#include <string>
+#include <vector>
+
+namespace EncryptionUtility {
+
+/**
+ * @brief Generates a cryptographically secure random nonce.
+ *
+ * @param length The desired length of the nonce in bytes (e.g., 16 for
+ * 128-bit).
+ *
+ * @return A string containing the nonce, in hexadecimal format.
+ * @throws std::runtime_error if nonce generation fails.
+ */
+const std::string generateCryptographicNonce(const std::size_t length);
+
+/**
+ * @brief Generates a random initialization vector (IV)
+ *
+ * This method generates a random initialization vector for cryptographic
+ * purposes.
+ *
+ * @param ivLength The desired length of the IV in bytes.
+ *
+ * @return A vector containing the IV generated.
+ * @throws std::runtime_error if IV generation fails.
+ */
+std::vector<uint8_t> generateRandomIV(std::size_t ivLength);
+
+/**
+ * @brief Encrypts a plaintext message using AES-256-CBC mode
+ *
+ * Encrypts a plaintext message using AES-256-CBC mode, using openssl library.
+ *
+ * @param plaintext The text to be encrypted.
+ * @param key The key to be used in the encryption process (32 bytes for
+ * AES-256).
+ * @param iv The initialization vector to be used in the encryption process
+ * (16 bytes).
+ *
+ * @return The ciphertext, in a hexadecimal string format.
+ * @throws std::runtime_error if IV or key size does not meet the requirements.
+ */
+std::string encryptMessageAes256CbcMode(const std::string &plaintext,
+                                        const std::vector<uint8_t> &key,
+                                        const std::vector<uint8_t> &iv);
+
+/**
+ * @brief Decrypts a ciphertext message using AES-256-CBC mode.
+ *
+ * Decrypts a ciphertext message using AES-256-CBC mode using the OpenSSL
+ * library.
+ *
+ * @param ciphertextHex The ciphertext in hexadecimal string format.
+ * @param key The key used in the encryption process (32 bytes for AES-256).
+ * @param iv The initialization vector used in the encryption process (16
+ * bytes).
+ *
+ * @return The decrypted plaintext as a standard string.
+ * @throws std::runtime_error if IV or key size is invalid or decryption fails.
+ */
+std::string decryptMessageAes256CbcMode(const std::string &ciphertextHex,
+                                        const std::vector<uint8_t> &key,
+                                        const std::vector<uint8_t> &iv);
+
+}; // namespace EncryptionUtility
+
+#endif // ENCRYPTION_UTILITY_HPP
---
+++ Cryptopals_resolutions/5-Set_5/cryptopals_set_5_problem_34/include/MessageExtractionFacility.hpp
@@ -0,0 +1,87 @@
+#ifndef MESSAGE_EXTRACTION_FACILITY_HPP
+#define MESSAGE_EXTRACTION_FACILITY_HPP
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <memory>
+#include <openssl/bn.h>
+#include <openssl/err.h>
+#include <string>
+#include <vector>
+
+namespace MessageExtractionFacility {
+
+struct BIGNUM_deleter {
+  void operator()(BIGNUM *bn) const { BN_free(bn); }
+};
+using UniqueBIGNUM = std::unique_ptr<BIGNUM, BIGNUM_deleter>;
+
+/**
+ * @brief This method will convert hexadecimal string to a vector of bytes.
+ *
+ * This method will convert hexadecimal string to a vector of bytes, using zero
+ * alignment.
+ *
+ * @param hexStr The input to be converted.
+ *
+ * @return The vector of bytes resulting of the conversion.
+ */
+std::vector<unsigned char> hexToBytes(const std::string &hexStr);
+
+/**
+ * @brief This method converts a vector of bytes into a string in hex format.
+ *
+ * This method will convert a vector of bytes into a string of hexadecimal
+ * characters, padded with zero.
+ *
+ * @param data The vector with bytes to be converted.
+ *
+ * @return A string containing the chars with hexadecimal format, zero padded.
+ */
+std::string toHexString(const std::vector<unsigned char> &data);
+
+/**
+ * @brief This method will convert an hexadecimal number to an unique big
+ * number.
+ *
+ * This method will convert an hexadecimal number to an unique big number.
+ *
+ * @param hexNumber The number in hexadecimal format.
+ *
+ * @return The number in an UniqueBIGNUM format.
+ * @throws std::runtime_error if conversion fails.
+ */
+UniqueBIGNUM hexToUniqueBIGNUM(const std::string &hexNumber);
+
+/**
+ * @brief This method will convert a number in a BIGNUM format to
+ * a hexadecimal format.
+ *
+ * This method will convert a number in a BIGNUM format to
+ * a hexadecimal format, performing all the calculations necessary.
+ *
+ * @param bn The number in a BIGNUM format.
+ *
+ * @return The number converted to a hexadecimal format.
+ * @throws std::runtime_error if conversion fails.
+ */
+std::string BIGNUMToHex(BIGNUM *bn);
+
+/**
+ * @brief This method will convert a number in a BIGNUM format to
+ * a decimal format.
+ *
+ * This method will convert a number in a BIGNUM format to
+ * a decimal format, performing all the calculations necessary.
+ *
+ * @param bn The number in a BIGNUM format.
+ *
+ * @return The number converted to a decimal format.
+ * @throws std::runtime_error if conversion fails.
+ */
+std::string BIGNUMToDec(BIGNUM *bn);
+
+}; // namespace MessageExtractionFacility
+
+#endif // MESSAGE_EXTRACTION_FACILITY_HPP
